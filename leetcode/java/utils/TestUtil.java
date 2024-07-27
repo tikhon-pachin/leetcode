163e2cdbea37c794660b74c460594ae1a5c3e6e1
Tikhon Pachin <tikhonpachin@gmail.com>
2024-07-27 13:24:33 -0500

    Setup leetcode java solutions

---
+++ leetcode/java/utils/TestUtil.java
@@ -0,0 +1,29 @@
+package leetcode.java.utils;
+
+import java.util.Arrays;
+
+public class TestUtil {
+    private static final String RESET = "\033[0m";
+    private static final String RED = "\033[0;31m";
+    private static final String GREEN = "\033[0;32m";
+
+    private TestUtil() {
+        throw new UnsupportedOperationException("TestUtil class should not be instantiated.");
+    }
+
+    public static String formatGreen(String output) {
+        return GREEN + output + RESET;
+    }
+
+    public static String formatRed(String output) {
+        return RED + output + RESET;
+    }
+
+    public static void test(String prefix, Boolean condition) {
+        System.out.println(prefix + (condition ? formatGreen("PASS") : formatRed("FAIL")));
+    }
+
+    public static Boolean areEqual(int[] array1, int[] array2) {
+        return Arrays.equals(array1, array2);
+    }
+}
